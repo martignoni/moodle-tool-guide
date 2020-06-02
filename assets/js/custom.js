f32183e3c4046c0c48279f574b2c3a4c2952d4b0
Nicolas Martignoni <nicolas@martignoni.net>
2020-06-02 23:21:53 +0200

    Display popup suggesting existing translation

---
+++ assets/js/custom.js
@@ -0,0 +1,35 @@
+// Open popup suggesting the detected user language
+
+function getBrowserLanguage() {
+  if (navigator.languages != undefined)
+    return navigator.languages[0].slice(0, 2);
+  else
+    return navigator.language.slice(0, 2);
+}
+userLanguage = getBrowserLanguage();
+
+var tour = new Tour({
+  name: "Show language",
+  debug: true,
+  storage: window.localStorage,
+  storage: false,
+  steps: [{
+      element: "#language-switch",
+      title: "Wrong language?",
+      content: "This page is available in other languages.",
+      placement: "bottom",
+      delay: [1500, 0],
+      duration: 10000,
+      reflex: true
+    }
+  ]
+});
+
+availableLanguages = ['en', 'fr', 'it', 'de'];
+// Check if browser language matches document language and language version exists.
+if (userLanguage !== document.documentElement.lang
+      && availableLanguages.includes(userLanguage)) {
+  // Action only if browser language is different and is available.
+  tour.init();
+  tour.start();
+}
